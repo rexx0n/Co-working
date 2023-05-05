3ab7b5d63c91f48c78124cbef06fc2a1aacc2cdd
rexx0n <alexey.mironenko46@gmail.com>
2023-05-05 12:41:26 +0300

    made carousel

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ import './scss/style.scss'
 //        document.getElementById('1').classList.add('questions__span--visible')
 //     })
 // })
-
+//Гамбургер
 let hamburger = document.querySelector('.hamburger__first > .hamburger__lines')
 let hamburger__first = document.querySelector('.hamburger__first')
 let hamburger__active = document.querySelector('.hamburger__active')
@@ -20,4 +20,24 @@ hamburger.addEventListener('click', function(e) {
 close.addEventListener('click', function(e) {
     hamburger__active.setAttribute('id', 'none')
     hamburger__first.removeAttribute('id')
+})
+
+//карусель
+let leftArrow = document.querySelector('.price__carousel-arrow--left')
+let rightArrow = document.querySelector('.price__carousel-arrow--right')
+let cards = document.querySelector('.price__cards')
+let counter = 0
+rightArrow.addEventListener('click', function (e) {
+    if (counter === -404) {
+        counter = 0
+    }
+    counter = counter + -404
+    cards.style.transform = `translateX(${counter}px)`
+})
+leftArrow.addEventListener('click', function (e) {
+    if (counter === 0) {
+        counter = -404
+    }
+    counter = 0
+    cards.style.transform = `translateX(${counter}px)`
 })
